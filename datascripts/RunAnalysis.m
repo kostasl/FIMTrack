f41b352c83b1a82f94278af677fb2b920bc6602b
kostasl <costaslag@gmail.com>
2016-01-28 16:00:57 +0000

    Added Section for v1190 genotype - and export to csv

--- datascripts/RunAnalysis.m
+++ datascripts/RunAnalysis.m
@@ -8,6 +8,11 @@ strDir = '/home/klagogia/Videos/FIMTrackVideos/OregonR/';
 strDir = '/home/klagogia/Videos/FIMTrackVideos/v1016 AbetaArc';
 [ABv1016_Dat,N,srcDir] = readDataForAnalysis(strDir);
 
+strDir = '/home/klagogia/Videos/FIMTrackVideos/v1190 APPTau';
+[v1190_Dat,N,srcDir] = readDataForAnalysis(strDir);
+
+
+
 %% EXTRACT DATA and ANALYSE for Mean Speeds
 speed_thres = 0;
 %Analyse WT
@@ -22,6 +27,13 @@ WT_OverallMeanSpeed  = nanmean(mean_velo)
 WT_OverallMedianSpeed = nanmedian(median_velo)
 %mean_velo = mean_velo(~isnan(mean_velo))
 
+%Translate Data column colNum to Data Source File
+%colNum = 1;
+%WT_list_velocities(colNum,:)
+%velo_wildtype.Properties.VarNames{colNum+1} 
+
+export(velo_wildtype,'File','WT_velocities.csv','WriteVarNames',true);
+
 %ABv1016 
 velo_ABv1016 = selectFeature(ABv1016_Dat, 'velo');
 
@@ -34,9 +46,26 @@ ABv1016_OverallMeanSpeed = nanmean(mean_velo)
 ABv1016_OverallMedianSpeed = nanmedian(median_velo)
 %mean_velo = mean_velo(~isnan(mean_velo))
 
+export(velo_ABv1016,'File','ABv1016_velocities.csv','WriteVarNames',true);
+
+%% v1190
+velo_v1190 = selectFeature(v1190_Dat, 'velo');
+
+v1190_list_velocities = double(velo_v1190(:,2:end));
+% list_velocities = list_velocities(tbl_velocities > speed_thres);
+mean_velo = nanmean(v1190_list_velocities(v1190_list_velocities>speed_thres));
+median_velo = nanmedian(v1190_list_velocities(v1190_list_velocities>speed_thres));
+
+v1190_OverallMeanSpeed = nanmean(mean_velo)
+v1190_OverallMedianSpeed = nanmedian(median_velo)
+%mean_velo = mean_velo(~isnan(mean_velo))
+
+export(velo_v1190,'File','v1190_velocities.csv','WriteVarNames',true);
+
 %% Produce Plots
 boxplot(WT_list_velocities);
 hist(WT_list_velocities,20);
+
 
 
 figure;
